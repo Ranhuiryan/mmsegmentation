e061f362001ae85c4a003c354490209da7c9311d
Ranhuiryan <qdgjhrh@outlook.com>
2022-06-23 12:34:27 +0800

    modify log collector

--- .dev/log_collector/log_collector.py
+++ .dev/log_collector/log_collector.py
@@ -42,10 +42,11 @@ def main():
     args = parse_args()
     cfg = load_config(args.config_dir)
     work_dir = cfg['work_dir']
-    metric = cfg['metric']
+    metrics = cfg['metrics']
+    important_metric = cfg['important_metric']
     log_items = cfg.get('log_items', [])
     ignore_keywords = cfg.get('ignore_keywords', [])
-    other_info_keys = cfg.get('other_info_keys', [])
+    # other_info_keys = cfg.get('other_info_keys', [])
     markdown_file = cfg.get('markdown_file', None)
     json_file = cfg.get('json_file', None)
 
@@ -56,13 +57,13 @@ def main():
 
     assert not (log_items and ignore_keywords), \
         'log_items and ignore_keywords cannot be specified at the same time'
-    assert metric not in other_info_keys, \
-        'other_info_keys should not contain metric'
+    assert important_metric in metrics, \
+        'important_metric should contain in metrics'
 
     if ignore_keywords and isinstance(ignore_keywords, str):
         ignore_keywords = [ignore_keywords]
-    if other_info_keys and isinstance(other_info_keys, str):
-        other_info_keys = [other_info_keys]
+    if metrics and isinstance(metrics, str):
+        metrics = [metrics]
     if log_items and isinstance(log_items, str):
         log_items = [log_items]
 
@@ -101,20 +102,23 @@ def main():
                         break
 
         new_log_dict = dict(
-            method=config_dir, metric_used=metric, last_iter=last_iter)
+            method=config_dir, important_metric=important_metric, last_iter=last_iter)
         for index, log in enumerate(val_list, 1):
             new_ordered_dict = OrderedDict()
             new_ordered_dict['eval_index'] = index
-            new_ordered_dict[metric] = log[metric]
-            for key in other_info_keys:
+            new_ordered_dict[important_metric] = log[important_metric]
+            for key in metrics:
                 if key in log:
                     new_ordered_dict[key] = log[key]
+                else:
+                    new_ordered_dict[key] = None
             val_list[index - 1] = new_ordered_dict
 
         assert len(val_list) >= 1, \
             f"work dir {config_dir} doesn't contain any evaluation."
         new_log_dict['last eval'] = val_list[-1]
-        new_log_dict['best eval'] = max(val_list, key=lambda x: x[metric])
+        # new_log_dict['best eval'] = val_list
+        new_log_dict['best eval'] = max(val_list, key=lambda x: x[important_metric])
         experiment_info_list.append(new_log_dict)
         print(f'{config_dir} is processed')
 
@@ -126,13 +130,17 @@ def main():
         lines_to_write = []
         for index, log in enumerate(experiment_info_list, 1):
             lines_to_write.append(
-                f"|{index}|{log['method']}|{log['best eval'][metric]}"
-                f"|{log['best eval']['eval_index']}|"
-                f"{log['last eval'][metric]}|"
+                f"|{index}|{log['method']}|"
+                f"{''.join([str(log['best eval'][metric])+'|' for metric in metrics])}"
+                f"{log['best eval']['eval_index']}|"
+                f"{''.join([str(log['last eval'][metric])+'|' for metric in metrics])}"
                 f"{log['last eval']['eval_index']}|{log['last_iter']}|\n")
         with open(markdown_file, 'w') as f:
-            f.write(f'|exp_num|method|{metric} best|best index|'
-                    f'{metric} last|last index|last iter num|\n')
+            f.write(f'|exp_num|method|'
+                    f'{"".join([metric + " best|" for metric in metrics])}'
+                    f'best index|'
+                    f'{"".join([metric + " last|" for metric in metrics])}'
+                    f'last index|last iter num|\n')
             f.write('|:---:|:---:|:---:|:---:|:---:|:---:|:---:|\n')
             f.writelines(lines_to_write)
 
---
+++ .dev/log_collector/log_collector_config.py
@@ -0,0 +1,35 @@
+work_dir = '../../work_dirs/mmseg_results_tb_1'
+metrics = ["aAcc", "mAcc", "mIoU", "mDice", "IoU.ground", "Dice.ground", "IoU.fissure", "Dice.fissure", "Acc.ground", "Acc.fissure"]
+important_metric = "aAcc"
+
+# metric = "aAcc"
+# should not include metric
+# other_info_keys = ["mAcc", "mIoU", "mDice", "IoU.ground", "Dice.ground", "IoU.fissure", "Dice.fissure", "Acc.ground", "Acc.fissure"]
+
+# specify the log files we would like to collect in `log_items`
+log_items = [
+    "20220612-151550_pspnet_r50-d8_256x256_80k_irfissure_fusion_60",
+    "20220612-200437_fcn_unet_s5-d16_256x256_40k_irfissure_fusion_60",
+    "20220613-004019_fcn_unet_s5-d16_256x256_40k_irfissure_thermal_60",
+    "20220613-051638_fcn_unet_s5-d16_256x256_40k_irfissure_thumb_60",
+    "20220613-132236_upernet_r50_256x256_80k_irfissure_fusion_60",
+    "20220613-180512_upernet_r50_256x256_80k_irfissure_thermal_60",
+    "20220613-224739_upernet_r50_256x256_80k_irfissure_thumb_60",
+    "20220614-084810_deeplabv3plus_r50-d8_256x256_80k_irfissure_fusion_60",
+    "20220614-135717_deeplabv3plus_r50-d8_256x256_80k_irfissure_thumb_60",
+    "20220614-190750_deeplabv3plus_r50-d8_256x256_80k_irfissure_thermal_60",
+    "20220619-230214_ocrnet_r50-d8_256x256_80k_b16_irfissure_fusion_60",
+    "20220620-032250_ocrnet_r50-d8_256x256_80k_b16_irfissure_thermal_60",
+    "20220620-074334_ocrnet_r50-d8_256x256_80k_b16_irfissure_thumb_60",
+    "20220620-131016_upernet_vit-b8_ln_mln_256x256_160k_irfissure_fusion_60",
+    "20220621-025047_upernet_vit-b8_ln_mln_256x256_160k_irfissure_thumb_60",
+    "20220621-163021_upernet_vit-b8_ln_mln_256x256_160k_irfissure_thermal_60",
+    "pspnet_r50-d8_256x256_80k_irfissure_thermal_60",
+    "pspnet_r50-d8_256x256_80k_irfissure_thumb_60"
+]
+# or specify ignore_keywords, then the folders whose name contain
+# `'segformer'` won't be collected
+# ignore_keywords = ['segformer']
+
+markdown_file = work_dir+'/markdowns/lr_in_trans.json.md'
+json_file = work_dir+'/jsons/trans_in_cnn.json'
